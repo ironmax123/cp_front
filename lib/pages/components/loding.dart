5279c2ac6c6a007babdea5e1f0bf8980d9a9952b
eita <>
2024-12-26 14:57:16 +0900

    背景削除

--- lib/pages/components/loding.dart
+++ lib/pages/components/loding.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 
 ///ロード中のダイアログ
+
 void showLoadingDialog(BuildContext context) {
   showDialog(
     context: context,
     barrierDismissible: false,
     builder: (context) {
-      return const AlertDialog(
-        content: SizedBox(
-          height: 80,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircularProgressIndicator(),
-              SizedBox(height: 16),
-              Text('データを取得中...'),
-            ],
-          ),
+      return const Dialog(
+        backgroundColor: Colors.transparent,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(height: 16),
+            Text(
+              'データを取得中...',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+          ],
         ),
       );
     },
